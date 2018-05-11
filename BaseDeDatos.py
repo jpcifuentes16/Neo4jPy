9768acb700e57d57b2b667e52ec00682ea5012a6
OJP98 <ojuarez_p@hotmail.com>
2018-05-11 06:39:45 -0600

    Ejemplo bd

--- BaseDeDatos.py
+++ BaseDeDatos.py
@@ -17,15 +17,19 @@ doctor.add(Oscar,Jose,Javier)
 
 # Paciente 1
 
-p1 = db.nodes.create(name="Paciente1", numero= "8888")
+p1 = db.nodes.create(Name="Paciente1", Numero= "8888")
 paciente.add(p1)
+
+m1 = db.nodes.create(Name="Aspirina",desdeFecha="110518",hastaFecha="180518",Dosis="1 C/8 horas")
  
 p1.relationships.create("Visits", Oscar)
+m1.relationships.create("Takes",p1)
+Oscar.relationships.create("Prescribes",p1)
 
 
 # Paciente 2
 
-p2 = db.nodes.create(name="Paciente2", numero= "7777")
+p2 = db.nodes.create(Name="Paciente2", Numero= "7777")
 paciente.add(p2)
  
 p2.relationships.create("Visits", Javier)
@@ -34,3 +38,4 @@ p2.relationships.create("Visits", Javier)
 #Paciente 1 conoce a paciente 2
 
 p1.relationships.create("Knows",p2)
+
